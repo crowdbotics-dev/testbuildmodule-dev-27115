899c9a67d5c063b839edcbe584a4d0662daa4748
Crowdbotics <engineering@crowdbotics.com>
2022-07-28 09:56:39 +0000

    remove package blargparse

--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ def home(request):
     packages = [
 	{'name':'blasy', 'url': 'http://pypi.python.org/pypi/blasy/0.1.2'},
 	{'name':'BlastRadius', 'url': 'http://pypi.python.org/pypi/BlastRadius/0.1.6'},
-	{'name':'blargparse', 'url': 'http://pypi.python.org/pypi/blargparse/0.0.3'},
 	{'name':'chainercmd', 'url': 'http://pypi.python.org/pypi/chainercmd/3.1.0a2'},
 	{'name':'chainer-chemistry', 'url': 'http://pypi.python.org/pypi/chainer-chemistry/0.1.0'},
 	{'name':'chainerboard', 'url': 'http://pypi.python.org/pypi/chainerboard/0.1.5'},
